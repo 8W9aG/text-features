c3064d7533704718742fb6e4bcb04afd43983589
Will Sackfield <will.sackfield@gmail.com>
2025-05-30 21:25:37 -0400

    Download NLTK models

--- textfeats/process.py
+++ textfeats/process.py
@@ -1,5 +1,6 @@
 """The main process function."""
 
+import nltk  # type: ignore
 import pandas as pd
 
 from .count_process import count_process
@@ -14,6 +15,7 @@ def process(
     key_words: set[str],
 ) -> pd.DataFrame:
     """Process the dataframe for text features."""
+    nltk.download("punkt_tab")
     df = embedding_process(df, verbose)
     df = stemmer_process(df, verbose, key_words)
     df = sentiment_process(df, verbose)
